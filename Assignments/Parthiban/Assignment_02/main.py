c443c99defeda155bc1cb0c0c52e5b0d16a7f673
PARTHIBANMURALI <73643401+PARTHIBANMURALI@users.noreply.github.com>
2022-10-12 12:13:28 +0530

    Add files via upload

---
+++ Assignments/Parthiban/Assignment_02/main.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("signin.html")
+@app.route('/signup')
+def sign_up():
+    return render_template("signup.html")
+@app.route('/home')
+def ho():
+    return render_template("home.html")
+@app.route('/about')
+def about():
+    return render_template("about.html")
+if __name__ == '__main__':
+    app.run(debug=True)
